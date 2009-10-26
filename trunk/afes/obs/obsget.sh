c8a81eea90b0e8b5a4e40f74a687d2de4f3a5656
takemiyo <takemiyo@a36015c4-1569-11de-a4db-e31d5fa7c4f0>
2009-10-26 17:31:57 +0000

    afes/obs/obsget.sh: upgraded for ncar/dss

--- trunk/afes/obs/obsget.sh
+++ trunk/afes/obs/obsget.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 ### Running on LINUX
 #
-# DIR
-CDIR=`pwd`
-cd ..
-AFES=`pwd`
-OBSDIR=$AFES/DATA/obs
-DLDIR=$OBSDIR/downloads
-SAVEDIR=$OBSDIR/prepbufr
-GRABBUFR=/export/data/dvl0/miyoshi/downloads/grabbufr
-BUFRLIB=/export/data/dvl0/miyoshi/downloads/BUFRLIB
+# DATA SOURCE
+#
+DATASRC=2 #1:NOAA/NOMADS, 2:UCAR/DSS
+#
+# COMPILER
+#
 F90=pgf90
 FFLAGS="-byteswapio" #big-endian IO
+#
+# TOOLS: grabbufr, BUFRLIB
+#
+GRABBUFR=/export/data/dvl0/miyoshi/downloads/grabbufr
+BUFRLIB=/export/data/dvl0/miyoshi/downloads/BUFRLIB
 # Initial time
 IY=2008
 IM=01
@@ -23,6 +25,15 @@ EM=01
 ED=01
 EH=00
 #
+# DIR
+#
+CDIR=`pwd`
+cd ..
+AFES=`pwd`
+OBSDIR=$AFES/DATA/obs
+DLDIR=$OBSDIR/downloads
+SAVEDIR=$OBSDIR/prepbufr
+#
 source $AFES/../common/timeinc.sh
 #
 # WKDIR
@@ -32,10 +43,38 @@ rm -rf $WKDIR
 mkdir -p $WKDIR
 cd $WKDIR
 #
-# mkdir
+# Settings for DATASRC
 #
+if test $DATASRC -eq 1
+then
+DATAURL="http://nomads.ncdc.noaa.gov/data/gdas"
+WGET="wget"
+DLDIR=$OBSDIR/downloads/nomads
+SAVEDIR=$OBSDIR/prepbufr/nomads
 mkdir -p $DLDIR
 mkdir -p $SAVEDIR
+else
+EMAIL=$1
+PASSWD=$2
+if test $# -ne 2
+then
+echo "USAGE: $0 EMAIL PASSWD"
+exit
+fi
+DATAURL="https://dss.ucar.edu/datazone/dsszone/ds337.0"
+WGET="wget --no-check-certificate"
+DLDIR=$OBSDIR/downloads/ucar
+SAVEDIR=$OBSDIR/prepbufr/ucar
+mkdir -p $DLDIR
+mkdir -p $SAVEDIR
+cd $DLDIR
+rm -f auth.dss_ucar_edu
+OPT1='-O /dev/null --save-cookies auth.dss_ucar_edu --post-data'
+OPT2="email=${EMAIL}&passwd=${PASSWD}&action=login"
+$WGET $OPT1="$OPT2" https://dss.ucar.edu/cgi-bin/login
+OPT="-N --load-cookies auth.dss_ucar_edu"
+cd $WKDIR
+fi
 #
 # build grabbufr
 #
@@ -62,17 +101,32 @@ echo " >>>"
 # GET NCEP PREPBUFR
 #
 cd $DLDIR
-DATAURL="http://nomads.ncdc.noaa.gov/data/gdas"
-if test ! -f $DLDIR/$IY$IM$ID$IH.prepbufr.nr
+if test $DATASRC -eq 1
 then
-wget $DATAURL/$IY$IM/$IY$IM$ID/gdas1.t${IH}z.prepbufr.nr
+DATAF=$IY$IM$ID$IH.prepbufr.nr
+else
+DATAF=prepbufr.gdas.$IY$IM$ID$IH.wo40
+fi
+if test ! -f $DATAF
+then
+if test $DATASRC -eq 1
+then
+$WGET $DATAURL/$IY$IM/$IY$IM$ID/gdas1.t${IH}z.prepbufr.nr
 mv gdas1.t${IH}z.prepbufr.nr $IY$IM$ID$IH.prepbufr.nr
+else
+$WGET $OPT $DATAURL/prepbufr.$IY$IM$ID.wo40.tar.gz
+tar zxvf prepbufr.$IY$IM$ID.wo40.tar.gz
+rm prepbufr.$IY$IM$ID.wo40.tar.gz
+mv $IY$IM$ID.wo40/* .
+rmdir $IY$IM$ID.wo40
+rm -rf $IY$IM$ID.wo40.tar.gz
+fi
 fi
 #
 # DECODE
 #
 cd $WKDIR
-ln -s $DLDIR/$IY$IM$ID$IH.prepbufr.nr prepbufr.tmp
+ln -s $DLDIR/$DATAF prepbufr.tmp
 wc -c prepbufr.tmp | ./grabbufr prepbufr.tmp prepbufr.in
 time ./decoder
 #
@@ -91,4 +145,5 @@ IM=$TM
 ID=$TD
 IH=$TH
 done
+rm -f $DLDIR/auth.dss_ucar_edu
 echo "NORMAL END"
