a8db33fe5a2db1a7020164c284786dc0fdd3bd29
takemiyo <takemiyo@a36015c4-1569-11de-a4db-e31d5fa7c4f0>
2010-04-15 13:49:59 +0000

    tagging alera2.0

---
+++ common/common_letkf.f90
@@ -0,0 +1,156 @@
+MODULE common_letkf
+!=======================================================================
+!
+! [PURPOSE:] Local Ensemble Transform Kalman Filtering (LETKF)
+!            Model Independent Core Module
+!
+! [REFERENCES:]
+!  [1] Ott et al., 2004: A local ensemble Kalman filter for atmospheric
+!    data assimilation. Tellus, 56A, 415-428.
+!  [2] Hunt et al., 2007: Efficient Data Assimilation for Spatiotemporal
+!    Chaos: A Local Ensemble Transform Kalman Filter. Physica D, 230,
+!    112-126.
+!
+! [HISTORY:]
+!  01/21/2009 Takemasa Miyoshi  Created at U. of Maryland, College Park
+!
+!=======================================================================
+!$USE OMP_LIB
+  USE common
+  USE common_mtx
+
+  IMPLICIT NONE
+
+  PUBLIC
+!=======================================================================
+!  LEKF Model Independent Parameters
+!=======================================================================
+  INTEGER,PARAMETER :: nbv=20    ! ensemble size
+
+CONTAINS
+!=======================================================================
+!  Main Subroutine of LETKF Core
+!   INPUT
+!     nobs             : array size, but only first nobsl elements are used
+!     nobsl            : total number of observation assimilated at the point
+!     hdxb(nobs,nbv)   : obs operator times fcst ens perturbations
+!     rdiag(nobs)      : observation error variance
+!     dep(nobs)        : observation departure (yo-Hxb)
+!     parm_infl        : covariance inflation parameter
+!   OUTPUT
+!     trans(nbv,nbv) : transformation matrix
+!=======================================================================
+SUBROUTINE letkf_core(nobs,nobsl,hdxb,rdiag,dep,parm_infl,trans)
+  IMPLICIT NONE
+  INTEGER,INTENT(IN) :: nobs
+  INTEGER,INTENT(IN) :: nobsl
+  REAL(r_size),INTENT(IN) :: hdxb(1:nobs,1:nbv)
+  REAL(r_size),INTENT(IN) :: rdiag(1:nobs)
+  REAL(r_size),INTENT(IN) :: dep(1:nobs)
+  REAL(r_size),INTENT(IN) :: parm_infl
+  REAL(r_size),INTENT(OUT) :: trans(nbv,nbv)
+  REAL(r_size) :: hdxb_rinv(nobsl,nbv)
+  REAL(r_size) :: eivec(nbv,nbv)
+  REAL(r_size) :: eival(nbv)
+  REAL(r_size) :: pa(nbv+1,nbv)
+  REAL(r_size) :: work1(nbv,nbv)
+  REAL(r_size) :: work2(nbv+1,nobsl)
+  REAL(r_size) :: work3(nbv)
+  REAL(r_size) :: rho
+  INTEGER :: i,j,k
+!-----------------------------------------------------------------------
+!  hdxb Rinv
+!-----------------------------------------------------------------------
+  DO j=1,nbv
+    DO i=1,nobsl
+      hdxb_rinv(i,j) = hdxb(i,j) / rdiag(i)
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  hdxb^T Rinv hdxb
+!-----------------------------------------------------------------------
+  DO j=1,nbv
+    DO i=1,nbv
+      work1(i,j) = hdxb_rinv(1,i) * hdxb(1,j)
+      DO k=2,nobsl
+        work1(i,j) = work1(i,j) + hdxb_rinv(k,i) * hdxb(k,j)
+      END DO
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  hdxb^T Rinv hdxb + (m-1) I / rho (covariance inflation)
+!-----------------------------------------------------------------------
+  rho = 1.0d0 / (1.0d0 + parm_infl)
+  DO i=1,nbv
+    work1(i,i) = work1(i,i) + REAL(nbv-1,r_size) * rho
+  END DO
+!-----------------------------------------------------------------------
+!  eigenvalues and eigenvectors of [ hdxb^T Rinv hdxb + (m-1) I ]
+!-----------------------------------------------------------------------
+  CALL mtx_eigen(1,nbv,work1,eival,eivec,i)
+!-----------------------------------------------------------------------
+!  Pa = [ hdxb^T Rinv hdxb + (m-1) I ]inv
+!-----------------------------------------------------------------------
+  DO j=1,nbv
+    DO i=1,nbv
+      work1(i,j) = eivec(i,j) / eival(j)
+    END DO
+  END DO
+  DO j=1,nbv
+    DO i=1,nbv
+      pa(i,j) = work1(i,1) * eivec(j,1)
+      DO k=2,nbv
+        pa(i,j) = pa(i,j) + work1(i,k) * eivec(j,k)
+      END DO
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  Pa hdxb_rinv^T
+!-----------------------------------------------------------------------
+  DO j=1,nobsl
+    DO i=1,nbv
+      work2(i,j) = pa(i,1) * hdxb_rinv(j,1)
+      DO k=2,nbv
+        work2(i,j) = work2(i,j) + pa(i,k) * hdxb_rinv(j,k)
+      END DO
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  Pa hdxb_rinv^T dep
+!-----------------------------------------------------------------------
+  DO i=1,nbv
+    work3(i) = work2(i,1) * dep(1)
+    DO j=2,nobsl
+      work3(i) = work3(i) + work2(i,j) * dep(j)
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  T = sqrt[(m-1)Pa]
+!-----------------------------------------------------------------------
+  DO j=1,nbv
+    rho = SQRT( REAL(nbv-1,r_size) / eival(j) )
+    DO i=1,nbv
+      work1(i,j) = eivec(i,j) * rho
+    END DO
+  END DO
+  DO j=1,nbv
+    DO i=1,nbv
+      trans(i,j) = work1(i,1) * eivec(j,1)
+      DO k=2,nbv
+        trans(i,j) = trans(i,j) + work1(i,k) * eivec(j,k)
+      END DO
+    END DO
+  END DO
+!-----------------------------------------------------------------------
+!  T + Pa hdxb_rinv^T dep
+!-----------------------------------------------------------------------
+  DO j=1,nbv
+    DO i=1,nbv
+      trans(i,j) = trans(i,j) + work3(i)
+    END DO
+  END DO
+
+  RETURN
+END SUBROUTINE letkf_core
+
+END MODULE common_letkf
